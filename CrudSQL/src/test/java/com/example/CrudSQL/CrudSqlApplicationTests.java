8b261c2832816851f3330c62e214bacf7ef8ea0a
darwinfnavarro <darwinfnavarro@gmail.com>
2024-09-04 14:33:38 -0400

    New Spring Boot project with database connection

---
+++ CrudSQL/src/test/java/com/example/CrudSQL/CrudSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
